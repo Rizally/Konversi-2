0b54391e17cd1f9a21400a81cf0d963443ec3115
Rizally <127819572+Rizally@users.noreply.github.com>
2023-03-28 10:55:24 +0700

    Add files via upload

---
+++ Konversi 2.py
@@ -0,0 +1,8 @@
+# Meminta masukan suhu dalam Celsius dari pengguna
+celsius = float(input("Masukkan suhu dalam Celsius: "))
+
+# Menghitung suhu dalam Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Menampilkan hasil konversi
+print(f"{celsius} derajat Celsius sama dengan {fahrenheit} derajat Fahrenheit.")
